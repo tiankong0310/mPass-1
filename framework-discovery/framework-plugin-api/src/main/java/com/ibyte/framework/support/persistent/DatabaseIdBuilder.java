623184d9cf5d2035630293a9610290e91cd85db2
Li.Shangzhi <13680348517@163.com>
2019-10-29 16:34:57 +0800

    数据ID标识

---
+++ framework-discovery/framework-plugin-api/src/main/java/com/ibyte/framework/support/persistent/DatabaseIdBuilder.java
@@ -0,0 +1,13 @@
+package com.ibyte.framework.support.persistent;
+
+/**
+ * @Description: <数据ID标识>
+ *
+ * @author li.Shangzhi
+ * @Date: 2019-10-29
+ */
+public interface DatabaseIdBuilder {
+
+
+    String getDatabaseId();
+}
