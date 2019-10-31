a62308cefcb9fa760350dfcb51319d122ba49d5d
Li.Shangzhi <13680348517@163.com>
2019-10-31 15:42:57 +0800

    系统配置常量

---
+++ framework-discovery/framework-discovery-api/src/main/java/com/ibyte/framework/discovery/constant/SystemConfigConstant.java
@@ -0,0 +1,24 @@
+package com.ibyte.framework.discovery.constant;
+
+/**
+ * @Description: <系统配置常量>
+ *
+ * @author li.Shangzhi
+ * @Date: 2019-10-31
+ */
+public interface SystemConfigConstant {
+    /**
+     * 配置所属分支
+     */
+    String CONFIG_LABEL = "master";
+
+    /**
+     * 系统配置使用场景
+     */
+    String CONFIG_PROFILE = "default";
+
+    /**
+     * 系统配置作用的应用名
+     */
+    String CONFIG_APPLICATION = "global";
+}
